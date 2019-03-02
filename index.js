6a3cb5ebd6da2b425d6d781594c02bb76c8a08f1
JazzK014 <44273728+JazzK014@users.noreply.github.com>
2019-03-02 13:27:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-/* Who wims */
+/* Who wins */
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
 }
